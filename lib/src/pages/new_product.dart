b3a61db4398c04b3877fee6ba2772b373e54bd63
FabrizioCozza <fabrizio.cozza@gmail.com>
2022-05-24 17:47:43 -0300

    creacion producto

---
+++ lib/src/pages/new_product.dart
@@ -0,0 +1,98 @@
+import 'package:envios_ya/src/models/product.dart';
+import 'package:envios_ya/src/services/server.dart';
+import 'package:flutter/material.dart';
+
+class NewProductPage extends StatefulWidget {
+  const NewProductPage({Key? key}) : super(key: key);
+
+  @override
+  State<NewProductPage> createState() => _NewProductPageState();
+}
+
+class _NewProductPageState extends State<NewProductPage> {
+  String? _name;
+  double? _price;
+  String? _owner;
+
+  final _createProductFormKey = GlobalKey<FormState>();
+  bool _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Nuevo Producto'),
+      ),
+      body: SafeArea(
+          child: SingleChildScrollView(
+              child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: _productForm(),
+      ))),
+    );
+  }
+
+  Widget _productForm() {
+    return Form(
+      key: _createProductFormKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextFormField(
+            onSaved: (value) => _name = value,
+            textInputAction: TextInputAction.next,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Name',
+            ),
+          ),
+          const SizedBox(height: 16.0),
+          TextFormField(
+            onSaved: (value) => _price = double.parse(value!),
+            textInputAction: TextInputAction.next,
+            keyboardType: const TextInputType.numberWithOptions(decimal: true),
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Price',
+            ),
+          ),
+          const SizedBox(height: 16.0),
+          TextFormField(
+            onSaved: (value) => _owner = value,
+            textInputAction: TextInputAction.done,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Owner',
+            ),
+          ),
+          const SizedBox(height: 16.0),
+          _isLoading
+              ? const CircularProgressIndicator()
+              : ElevatedButton(
+                  onPressed: () async => _createProduct(),
+                  child: const Text("Crear producto"))
+        ],
+      ),
+    );
+  }
+
+  void _createProduct() async {
+    setState(() {
+      _isLoading = true;
+    });
+    FocusScope.of(context).unfocus();
+    if (_createProductFormKey.currentState!.validate()) {
+      _createProductFormKey.currentState!.save();
+      try {
+        await Server.createProduct(Product(_name!, _price!, _owner!));
+      } on ServerException catch (e) {
+        if (!mounted) return;
+        final snackBar = SnackBar(content: Text(e.message));
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      }
+    }
+    setState(() {
+      _isLoading = false;
+    });
+  }
+}
