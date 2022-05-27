44deb9331190d4e986b07c168030fd206bf6137a
FabrizioCozza <fabrizio.cozza@gmail.com>
2022-05-26 23:49:36 -0300

    product list

--- lib/src/models/product.dart
+++ lib/src/models/product.dart
@@ -1,13 +1,19 @@
 class Product {
+  Product({required this.name, required this.price, required this.owner});
+
   String name;
   double price;
   String owner;
-
-  Product(this.name, this.price, this.owner);
 
   Map<String, dynamic> toJson() => {
         'name': name,
         'price': price.toDouble(),
         'owner': owner,
       };
+
+  factory Product.fromJson(Map<String, dynamic> json) => Product(
+        name: json['name'],
+        price: json['price'],
+        owner: json['owner'],
+      );
 }
