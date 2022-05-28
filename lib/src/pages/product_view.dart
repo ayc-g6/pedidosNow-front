47a8031af3a8adf02f73babf0a154dabe586f25c
micaelavillordo <mvillordo@fi.uba.ar>
2022-05-28 01:09:09 -0300

    [UPDATE] - Changed visual details on the ProductViewPage

--- lib/src/pages/product_view.dart
+++ lib/src/pages/product_view.dart
@@ -19,41 +19,42 @@ class _ProductViewPageState extends State<ProductViewPage> {
         automaticallyImplyLeading: true,
       ),
       body: Container(
-        height: 150,
+        height: 200,
         child: Card(
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: ListTile(
-                      title: Text(widget.product.name,
-                          style: Theme.of(context).textTheme.titleLarge),
-                      subtitle: Text("De: ${widget.product.owner}"),
-                    ),
-                  ),
-                  Expanded(
-                    child: Text("\$${widget.product.price}",
+            child: Column(
+              children: [
+                Expanded(
+                  child: ListTile(
+                    minVerticalPadding: 30,
+                    title: Text(widget.product.name,
                         style: Theme.of(context).textTheme.titleLarge),
-                  ),
-                ],
-              ),
-              Expanded(
-                child: Container(
-                  alignment: Alignment.bottomRight,
-                  margin: EdgeInsets.only(right: 5),
-                  child: ElevatedButton(
-                    child: Text("Ver información nutricional"),
-                    //TODO: go to te info nutri page
-                    onPressed: () => {},
+                    subtitle: Text("De: ${widget.product.owner}"),
                   ),
                 ),
-              ),
-            ],
-          ),
-          elevation: 8,
-          margin: EdgeInsets.all(10),
-        ),
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: Container(
+                        alignment: Alignment.bottomLeft,
+                        margin: EdgeInsets.only(left: 5),
+                        child: ElevatedButton(
+                          child: Text("Ver información nutricional"),
+                          //TODO: go to te info nutri page
+                          onPressed: () => {},
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Text("\$${widget.product.price}",
+                          style: Theme.of(context).textTheme.titleLarge),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            elevation: 8,
+            margin: EdgeInsets.only(bottom: 10)),
       ),
     );
   }
