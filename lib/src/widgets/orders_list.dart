aeec9877040808a1c53f1b9601a33a0558974318
Santiago Nicolás Czop <czopsantiago@gmail.com>
2022-06-06 02:45:10 -0300

    [UPDATE] - Changed Order Card to own file

--- lib/src/widgets/orders_list.dart
+++ lib/src/widgets/orders_list.dart
@@ -2,8 +2,8 @@ import 'package:envios_ya/src/models/auth.dart';
 import 'package:envios_ya/src/models/business.dart';
 import 'package:envios_ya/src/models/order.dart';
 import 'package:envios_ya/src/models/product.dart';
-import 'package:envios_ya/src/pages/order_view.dart';
 import 'package:envios_ya/src/services/server.dart';
+import 'package:envios_ya/src/widgets/order_card.dart';
 import 'package:flutter/material.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:provider/provider.dart';
@@ -105,84 +105,5 @@ class _OrdersListState extends State<OrdersList> {
   void dispose() {
     _pagingController.dispose();
     super.dispose();
-  }
-}
-
-class OrderCard extends StatelessWidget {
-  final Order order;
-  final Product product;
-  final Business business;
-
-  const OrderCard(
-      {Key? key,
-      required this.order,
-      required this.product,
-      required this.business})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) => OrderViewPage(
-              product: product,
-              order: order,
-              business: business,
-            ),
-          ),
-        );
-      },
-      child: Card(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Order #${order.id}',
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                  Text('Awaiting Confirmation',
-                      style: Theme.of(context).textTheme.labelLarge),
-                ],
-              ),
-              const SizedBox(height: 8.0),
-              const Divider(),
-              const SizedBox(height: 8.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('${order.quantity} x ${product.name}',
-                      style: Theme.of(context).textTheme.labelLarge),
-                  Text(
-                      '\$ ${(order.quantity * product.price).toStringAsFixed(2)}'),
-                ],
-              ),
-              const SizedBox(height: 8.0),
-              const Divider(),
-              const SizedBox(height: 8.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Total:', style: Theme.of(context).textTheme.labelLarge),
-                  Text(
-                    '\$ ${(order.quantity * product.price).toStringAsFixed(2)}',
-                    style: Theme.of(context)
-                        .textTheme
-                        .labelLarge!
-                        .copyWith(color: Colors.green),
-                  )
-                ],
-              )
-            ],
-          ),
-        ),
-      ),
-    );
   }
 }
